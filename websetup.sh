ec121815656a61355812692c228de7257f92333e
seunayolu <seunayolu@gmail.com>
2024-03-02 17:04:34 -0600

    deploy-stage-modified

--- websetup.sh
+++ websetup.sh
@@ -4,8 +4,8 @@
 echo "########################################"
 echo "Installing packages."
 echo "########################################"
-sudo apt update -y > /dev/null
-sudo apt install wget unzip apache2 -y > /dev/null
+#sudo apt update -y > /dev/null
+#sudo apt install wget unzip apache2 -y > /dev/null
 echo
 
 # Creating Temp Directory
@@ -16,9 +16,9 @@ mkdir -p /tmp/webfiles
 cd /tmp/webfiles
 echo
 
-wget https://www.tooplate.com/zip-templates/2133_moso_interior.zip > /dev/null
-unzip 2133_moso_interior.zip > /dev/null
-sudo cp -r 2133_moso_interior/* /var/www/html/
+wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip > /dev/null
+unzip 2137_barista_cafe.zip > /dev/null
+sudo cp -r 2137_barista_cafe/* /var/www/html/
 echo
 
 # Bounce Service
