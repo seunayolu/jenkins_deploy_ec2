1aabcbc7e72a4fa84dffd3b3e246782b189f0966
seunayolu <seunayolu@gmail.com>
2025-03-23 21:46:42 +0100

    template updated and json output saved

--- websetup.sh
+++ websetup.sh
@@ -16,9 +16,9 @@ mkdir -p /tmp/webfiles
 cd /tmp/webfiles
 echo
 
-wget https://www.tooplate.com/zip-templates/2130_waso_strategy.zip > /dev/null
-unzip 2130_waso_strategy.zip > /dev/null
-sudo cp -r 2130_waso_strategy/* /var/www/html/
+wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip > /dev/null
+unzip 2137_barista_cafe.zip > /dev/null
+sudo cp -r 2137_barista_cafe/* /var/www/html/
 echo
 
 # Bounce Service
