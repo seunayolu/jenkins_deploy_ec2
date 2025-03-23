cec0155444a3f199693f45fcbb63904c195af778
seunayolu <seunayolu@gmail.com>
2025-03-23 22:07:21 +0100

    another template

--- websetup.sh
+++ websetup.sh
@@ -16,9 +16,9 @@ mkdir -p /tmp/webfiles
 cd /tmp/webfiles
 echo
 
-wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip > /dev/null
-unzip 2137_barista_cafe.zip > /dev/null
-sudo cp -r 2137_barista_cafe/* /var/www/html/
+wget https://www.tooplate.com/zip-templates/2132_clean_work.zip > /dev/null
+unzip 2132_clean_work.zip > /dev/null
+sudo cp -r 2132_clean_work/* /var/www/html/
 echo
 
 # Bounce Service
