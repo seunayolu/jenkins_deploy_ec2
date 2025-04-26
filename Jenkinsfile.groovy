41903c22ef4c4e569eb37eda3f8dfb1a8ac171c8
seunayolu <seunayolu@gmail.com>
2025-04-26 13:55:54 +0100

    jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     parameters {
-        string(name: 'EC2_IP', defaultValue: '54.75.33.241', description: 'EC2 Instance IP Address')
+        string(name: 'EC2_IP', defaultValue: '172.31.34.253', description: 'EC2 Instance IP Address')
         string(name: 'GIT_BRANCH', defaultValue: 'jenkins', description: 'Git Branch to checkout')
         string(name: 'GIT_URL', defaultValue: 'https://github.com/seunayolu/jenkins_deploy_ec2.git', description: 'Git Repository URL')
         string(name: 'SSH_CREDENTIALS', defaultValue: 'ec2', description: 'SSH Credentials ID')
